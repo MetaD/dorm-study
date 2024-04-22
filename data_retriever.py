7687db61899d50b4b6c2158bf52e6a9cf7418f5e
Meng Du <meng.du@intel.com>
2024-04-22 15:14:30 -0700

    data retriever

---
+++ data_retriever.py
@@ -0,0 +1,26 @@
+# Install the firebase-admin package before running this script:
+#    pip3 install firebase-admin
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+
+if __name__ == '__main__':
+    # fetch data from firebase
+    cred = credentials.Certificate('serviceAccountKey.json')
+    firebase_admin.initialize_app(cred)
+    db = firestore.client()
+    subjects = db.collection('prod_data').stream()
+
+    # iterate over all subjects
+    for subj in subjects:
+        data = subj.to_dict()
+        times = [k for k in data.keys() if k.startswith('1')]
+        print('Subject ID:', subj.id)
+        print('Survey start times:', times)
+
+        # now you can explore the subject's data and do whatever you want with it
+        for t in times:
+            for i in range(5):
+                print(data[t][f'1.{i}'])
